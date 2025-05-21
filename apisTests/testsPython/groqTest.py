614295c1dfa63b6143175d241ad5d71839dad94d
Ian-Cerqueira <iandavi16@outlook.com>
2025-05-21 15:25:56 -0300

    codigo para chamar o groq

--- apisTests/testsPython/groqTest.py
+++ apisTests/testsPython/groqTest.py
@@ -1,1 +1,21 @@
-print('algo 10')
+from groq import Groq
+
+client = Groq(
+    api_key= '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',
+)
+
+client_message = ''
+
+while client_message != "tchau" :
+    client_message = input()
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"{client_message}",
+            }
+        ],
+        model="llama3-70b-8192",
+    )
+
+    print(chat_completion.choices[0].message.content)
