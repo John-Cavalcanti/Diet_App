d5d21ec147154c809100b128f85efa894f1cf9ed
Paula Oliveira <paulacaroliveiras@gmail.com>
2025-06-15 22:58:59 -0300

    feat: criando componente primary button

---
+++ frontend/nutreai/src/componens/primary-button.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from "react"
+import styled from "styled-components"
+
+interface PrimaryButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode
+}
+export function PrimaryButton({children, onClick}: PrimaryButtonProps){
+    return (
+        <Container onClick={onClick}>
+            {children}
+        </Container>
+    )
+}
+
+const Container = styled.button`
+    width: 100%;
+    background: ${({theme}) => theme["green-700"]};
+    color: white;
+    font-weight: 600;
+    font-size: 0.75rem;
+    padding-block: 0.75rem;
+    border: none;
+    border-radius: 8px;
+`
