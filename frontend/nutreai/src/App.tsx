6f826e41d1a5dc3fe1fad3c39910393124d59128
Paula Oliveira <paulacaroliveiras@gmail.com>
2025-06-15 22:53:13 -0300

    feat: criando estilizacao global

--- frontend/nutreai/src/App.tsx
+++ frontend/nutreai/src/App.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
