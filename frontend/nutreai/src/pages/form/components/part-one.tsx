d5060061bd631ade8efa00142e2f3220359ccb29
Paula Oliveira <paulacaroliveiras@gmail.com>
2025-06-15 23:11:04 -0300

    feat:  criando primaira parte do formulario

---
+++ frontend/nutreai/src/pages/form/components/part-one.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+import { FormCard } from "./form-card"
+import { FormInput } from "./form-inputs"
+export function PartOne() {
+    return (
+        <FormCard 
+            title="Vamos te conhecer melhor!" 
+            description="Informe alguns dados para criar seu plano alimentar. Essas informações são importantes para personalizar sua alimentação."
+        >
+            <InputsContainer>
+                <FormInput id="nome" label={"Nome"} placeholder="Nome" />
+                <FormInput id="email" label={"Email"} placeholder="Email" />
+                <FormInput id="dataNasc" label={"Data de nascimento"} placeholder="dd/mm/aaaa" type="date" />
+                <FormInput id="peso" label={"Peso"} placeholder="0.00kg" type="number" />
+                <FormInput id="altura" label={"Altura"} placeholder="Altura (centímetros)" />
+                <FormInput id="sexo" label={"Sexo"} placeholder="Sexo" />
+            </InputsContainer>
+        </FormCard>
+    )
+}
+
+const InputsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    margin-block: 2rem;
+
+    gap: 0.5rem;
+`
