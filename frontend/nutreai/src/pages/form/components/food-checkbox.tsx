797b36806b18046383fe1ee2eccd2627d2b7c6be
Paula Oliveira <paulacaroliveiras@gmail.com>
2025-06-16 17:58:49 -0300

    feat: criando componente food-checkbox

--- frontend/nutreai/src/pages/form/components/food-checkbox.tsx
+++ frontend/nutreai/src/pages/form/components/food-checkbox.tsx
@@ -1,15 +1,15 @@
-import { CheckboxIndicator } from "@radix-ui/react-checkbox";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import type { ReactNode } from "react";
 import styled from "styled-components";
 
 interface FoodCheckBoxProps {
     children: ReactNode
 }
-export function FoodCheckbox ({ children }: FoodCheckBoxProps){
+export function FoodCheckbox({ children }: FoodCheckBoxProps) {
     return (
-        <CheckboxIndicator asChild>
+        <Checkbox.Root asChild>
             <Button>{children}</Button>
-        </CheckboxIndicator>
+        </Checkbox.Root>
     )
 }
 
@@ -17,14 +17,23 @@ const Button = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 8rem;
+    width: 100%;
 
     padding-block: 0.5rem;
 
     font-size: 0.625rem;
 
-    border: 1px solid ${({theme}) => theme["green-700"]};
+    border: 1px solid ${({ theme }) => theme["green-700"]};
     border-radius: 18px;
     
     gap: 1rem;
+
+    &:hover {
+        background-color: ${({theme}) => theme["green-100"]};
+    }
+
+  &[data-state='checked'] {
+    background-color: ${({theme}) => theme["green-100"]};
+    border: 2px solid ${({ theme }) => theme["green-700"]};
+  }
 `
