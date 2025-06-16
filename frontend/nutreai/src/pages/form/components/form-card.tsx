8329f1d86f727bbf81a911004af0856005f2dd8a
Paula Oliveira <paulacaroliveiras@gmail.com>
2025-06-15 23:04:16 -0300

    feat: criando componente form-card

---
+++ frontend/nutreai/src/pages/form/components/form-card.tsx
@@ -0,0 +1,61 @@
+import type { ReactNode } from "react";
+import styled from "styled-components";
+import { PrimaryButton } from "../../../componens/primary-button";
+import { SecondaryButton } from "../../../componens/secondary-button";
+
+interface FormCardProps {
+    children: ReactNode
+    title: string
+    description: string
+}
+export function FormCard({ title, description, children } : FormCardProps){
+    return (
+        <Container>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+            {children}
+            <ButtonsContainer>
+                <SecondaryButton>Voltar</SecondaryButton>
+                <PrimaryButton>Próximo</PrimaryButton>
+            </ButtonsContainer>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    width: 25rem;
+    height: fit-content;
+    min-height: 30rem;
+
+    display: flex;
+    flex-direction: column;
+
+    margin: auto;
+    padding: 3.5rem;
+
+    background: white;
+
+    border-radius: 8px;
+    box-shadow: 3px 3px 3px 3px rgba(0, 0, 0, 0.2);
+`
+const Title = styled.h2`
+    font-size: 1rem;
+    font-weight: bold;
+
+    margin-bottom: 0.25rem;
+`
+
+const Description = styled.p`
+    color: ${({ theme }) => theme["text-color"]};
+    font-size: 0.75rem;
+
+    margin-bottom: 1rem;
+`
+
+const ButtonsContainer = styled.div`
+    display: flex;
+
+    margin-top: auto;
+
+    gap: 1.5rem;
+`
