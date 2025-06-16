5ec2d8a691428ab51a73bcd7ce5c797aa11049cc
Paula Oliveira <paulacaroliveiras@gmail.com>
2025-06-15 22:46:28 -0300

    feat: criando tema de estilizacao

--- frontend/nutreai/src/App.tsx
+++ frontend/nutreai/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ frontend/nutreai/src/styles/themes/default.ts
@@ -0,0 +1,8 @@
+export const defaultTheme = {
+    'background-color': '#EDF4F1',
+    'green-700': '#2B7A4B',
+    'green-100': '#C8E6C9',
+
+
+    'text-color': '#4A4A4A',
+}
