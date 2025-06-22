cfe76ce2a9fe6cccb5e75d03e89ab1e224fd8597
Ian-Cerqueira <iandavi16@outlook.com>
2025-06-22 12:55:49 -0300

    feat: adição do módulo de autenticacao e config ao imports do app module
    o configmodule é configurado como global para facilitar o acesso a variaveis de ambiente

--- system/backend/src/app.module.ts
+++ system/backend/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WeeklyDietModule } from './weekly-diet/weekly-diet.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, WeeklyDietModule],
+  imports: [
+    UsersModule,
+    WeeklyDietModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
