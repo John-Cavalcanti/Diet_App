8d59d18ce92ea32b1d633246b79f6ca06e7b0582
Ian-Cerqueira <iandavi16@outlook.com>
2025-06-29 12:47:50 -0300

    docs: adiciona descrições e exemplos para os endpoints login e signup

--- system/backend/src/auth/auth.controller.ts
+++ system/backend/src/auth/auth.controller.ts
@@ -4,41 +4,29 @@ import { Controller, Post, Get, HttpCode, HttpStatus, Body, Request, UseGuards }
 import { LogInDto } from './dto/log-in.dto';
 import { AuthGuard } from './auth.guard';
 import { SignUpDto } from './dto/sign-up.dto';
-import { ApiTags, ApiOperation, ApiAcceptedResponse, ApiOkResponse, ApiUnauthorizedResponse, ApiBadGatewayResponse, ApiBadRequestResponse } from '@nestjs/swagger';
-
+import { ApiTags } from '@nestjs/swagger';
+import { AuthSwaggerDocsLogIn } from './decorators/auth-swagger-login.decorators';
+import { AuthSwaggerDocsSignUp } from './decorators/auth-swagger-signup.decorators';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiOperation({
-    summary: 'Autentica o email e senha passados pelo usuário e retorna o token de acesso caso a autenticação não encontre nenhum erro.',
-  })
-  @ApiOkResponse({
-    description: 'Usuário passou email e senha válidos.',
-  })
-  @ApiUnauthorizedResponse({
-    description: 'Usuário passou email inexistente ou senha inválida / errada.',
-  })
-  @ApiBadRequestResponse({
-    description: 'Usuário usou tipos de dados não compatíveis com os campos email ou senha ou não seguiu os padrões de senha.' 
-  })
+  @AuthSwaggerDocsLogIn()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   logIn(@Body() logInDto: LogInDto) {
     return this.authService.logIn(logInDto);
 }
 
-  @ApiOperation({
-    summary: '',
-  })
   @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
   }
 
+  @AuthSwaggerDocsSignUp()
   @Post('signup')
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
