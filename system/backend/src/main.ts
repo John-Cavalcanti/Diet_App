32542d96199b082550fac204a00722b5aeb57a39
Ian-Cerqueira <iandavi16@outlook.com>
2025-06-29 09:29:29 -0300

    feat: adição inicial do Swagger a aplicação

--- system/backend/src/main.ts
+++ system/backend/src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000;
@@ -12,16 +13,28 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true
-    })
+      transform: true,
+    }),
   );
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     // credentials: true // descomentar quando for fazer autenticação
   });
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Nutre.AI')
+    .setDescription(
+      'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+    )
+    .setVersion('1.0')
+    .addTag('Auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document); // é necessário acessar o endereço localhost:port/api/swagger
+
   await app.listen(port);
 
   console.log('\nServer is running on port: http://localhost:' + port + '/api');
