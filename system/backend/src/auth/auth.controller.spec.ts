ab1a2329a22198861c871c6ff8c90f562c4c6a4e
Ian-Cerqueira <168128013+Ian-Cerqueira@users.noreply.github.com>
2025-07-02 16:12:12 +0000

    test: Adiciona as dependencias para teste de auth controller

--- system/backend/src/auth/auth.controller.spec.ts
+++ system/backend/src/auth/auth.controller.spec.ts
@@ -1,13 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const mockAuthService = {};
+  const mockAuthGuard = {};
+  const mockJwtService = {};
+  const mockConfigService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [AuthService, AuthGuard, JwtService, ConfigService],
+    })
+    .overrideProvider(AuthGuard)
+    .useValue(mockAuthGuard)
+    
+    .overrideProvider(JwtService)
+    .useValue(mockJwtService)
+
+    .overrideProvider(ConfigService)
+    .useValue(mockConfigService)
+
+    .overrideProvider(AuthService)
+    .useValue(mockAuthService)
+    .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
