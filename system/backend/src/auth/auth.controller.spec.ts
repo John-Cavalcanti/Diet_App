2f0425320c32f5b5ca6ce8f034e0c9a8fe42eae0
Ian-Cerqueira <iandavi16@outlook.com>
2025-07-02 22:23:13 -0300

    refact(test): adição de descrição para cada teste de cada caso
    também foram adicionados os dois primeiros testes para login

--- system/backend/src/auth/auth.controller.spec.ts
+++ system/backend/src/auth/auth.controller.spec.ts
@@ -18,6 +18,11 @@ describe('AuthController', () => {
               ...dto,
             });
           }),
+          logIn: jest.fn().mockImplementation(() => {
+            return {
+              acess_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImVtYWlsIjoiZmVybmFuZGEub2xpdmVpcmFAZXhhbXBsZS5jb20iLCJpYXQiOjE3NTEyMDk0NzgsImV4cCI6MTc1MTIxMzA3OH0.TN48QRk_GHKb6R1yqbXpgy05osvc3pKoMaVyNTNuoRs',
+            };
+          }), 
         };
   const mockAuthGuard = {};
   const mockJwtService = {};
@@ -48,63 +53,101 @@ describe('AuthController', () => {
     expect(controller).toBeDefined();
   });
   
-  it('deve cadastrar um usuário com sucesso', async () => {
-    expect( await controller.signUp(
-      {
-        name: 'Fernanda Oliveira',
-        email: 'fernanda.oliveira@example.com',
-        password: 'SenhaForte2024',
-        birthday: '1995-04-10',
-        weight: 60,
-        height: 167,
-        workoutsFrequency: '5 vezes por semana',
-        goals: 'Definir músculos e manter energia',
-        foodRestrictions: 'Lactose, nozes',
-        foodPreferences: 'Vegetariana, prefere pratos quentes no almoço'
-      }
-    )).toBeInstanceOf(User)
+  describe('POST /signup', () => {
+    describe('Quando os dados são válidos', () => { 
+      it('deve cadastrar um usuário com sucesso', async () => {
+        expect( await controller.signUp(
+          {
+            name: 'Fernanda Oliveira',
+            email: 'fernanda.oliveira@example.com',
+            password: 'SenhaForte2024',
+            birthday: '1995-04-10',
+            weight: 60,
+            height: 167,
+            workoutsFrequency: '5 vezes por semana',
+            goals: 'Definir músculos e manter energia',
+            foodRestrictions: 'Lactose, nozes',
+            foodPreferences: 'Vegetariana, prefere pratos quentes no almoço'
+          }
+        )).toBeInstanceOf(User)
+      });
+    });
+
+    describe('Quando cliente tentar criar uma conta com email já existente', () => {
+      it('deve emitir conflict error', async () => {
+        mockAuthService.signUp.mockImplementationOnce( async () => {
+          throw new ConflictException('Email já está em uso.');
+        });
+        
+        await expect(controller.signUp(
+          {
+            name: 'Fernanda Oliveira',
+            email: 'fernanda.oliveira@example.com',
+            password: 'SenhaForte2024',
+            birthday: '1995-04-10',
+            weight: 60,
+            height: 167,
+            workoutsFrequency: '5 vezes por semana',
+            goals: 'Definir músculos e manter energia',
+            foodRestrictions: 'Lactose, nozes',
+            foodPreferences: 'Vegetariana, prefere pratos quentes no almoço'
+          }
+        )).rejects.toThrow(ConflictException)
+      });
+    });
+
+    describe('Quando cliente tentar criar conta com tipos de dados não válidos', () => {
+      it('deve emitir bad request error', async () => {
+        mockAuthService.signUp.mockImplementationOnce( async () => {
+          throw new BadRequestException();
+        });
+        
+        await expect(controller.signUp(
+          {
+            name: 'Fernanda Oliveira',
+            email: 'fernanda.oliveira@example.com',
+            password: 'SenhaForte2024',
+            birthday: '1995-04-10',
+            weight: 60,
+            height: 167,
+            workoutsFrequency: '5 vezes por semana',
+            goals: 'Definir músculos e manter energia',
+            foodRestrictions: 'Lactose, nozes',
+            foodPreferences: 'Vegetariana, prefere pratos quentes no almoço' 
+          }
+        )).rejects.toThrow(BadRequestException)
+      });
+    });
   });
 
-  it('deve emitir conflict error', async () => {
-    mockAuthService.signUp.mockImplementationOnce( async () => {
-      throw new ConflictException('Email já está em uso.');
+  describe('POST /logIn', () => {
+    describe('Quando cliente acessar com email e senha válidos', () => {
+      it('deve retornar um token de acesso', async () => {
+        expect(controller.logIn(
+          {
+            email: 'fernanda.oliveira@example.com',
+            password: 'SenhaForte2024',
+          }
+        )).toBeDefined()
+      });
+    });
+  
+    describe('Quando cliente usa tipos de dados não permitidos', () => {
+      it('deve emitir bad request error', async () => {
+        mockAuthService.logIn.mockImplementationOnce( async () => {
+          throw new BadRequestException();
+        })
+        await expect(controller.logIn(
+          {
+            email: 'fernanda.oliveira@example.com',
+            password: 'SenhaForte2024', // passar ao inves de string com senha, um number, por exemplo
+          }
+        )).rejects.toThrow(BadRequestException)
+      });
     });
     
-    await expect(controller.signUp(
-      {
-        name: 'Fernanda Oliveira',
-        email: 'fernanda.oliveira@example.com',
-        password: 'SenhaForte2024',
-        birthday: '1995-04-10',
-        weight: 60,
-        height: 167,
-        workoutsFrequency: '5 vezes por semana',
-        goals: 'Definir músculos e manter energia',
-        foodRestrictions: 'Lactose, nozes',
-        foodPreferences: 'Vegetariana, prefere pratos quentes no almoço'
-      }
-    )).rejects.toThrow(ConflictException)
+  
   });
 
-  it('deve emitir bad request error', async () => {
-    mockAuthService.signUp.mockImplementationOnce( async () => {
-      throw new BadRequestException();
-    });
-    
-    await expect(controller.signUp(
-      {
-        name: 'Fernanda Oliveira',
-        email: 'fernanda.oliveira@example.com',
-        password: 'SenhaForte2024',
-        birthday: '1995-04-10',
-        weight: 60,
-        height: 167,
-        workoutsFrequency: '5 vezes por semana',
-        goals: 'Definir músculos e manter energia',
-        foodRestrictions: 'Lactose, nozes',
-        foodPreferences: 'Vegetariana, prefere pratos quentes no almoço' 
-      }
-    )).rejects.toThrow(BadRequestException)
-  });
 
 });
