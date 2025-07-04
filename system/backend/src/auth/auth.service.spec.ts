c7f896989c34b047cdea414e78b088fb763b7ac3
Ian-Cerqueira <iandavi16@outlook.com>
2025-07-04 11:33:07 -0300

    test: Adição inicial do test de Auth Service

--- system/backend/src/auth/auth.service.spec.ts
+++ system/backend/src/auth/auth.service.spec.ts
@@ -1,17 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { mockLogInAccessToken } from './constants/constants';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/entities/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   const mockJwtService = {
-    verifyAsync: jest.fn().mockResolvedValue(mockLogInAccessToken),
+    signAsync: jest.fn().mockResolvedValue(mockLogInAccessToken),
+  };
+
+  const mockUsersService = {
+    findByEmail: jest.fn().mockReturnValue(
+      new User({
+        name: 'Fernanda Oliveira',
+        email: 'fernanda.oliveira@example.com',
+        password: 'SenhaForte2024',
+        birthday: '1995-04-10',
+        weight: 60,
+        height: 167,
+        workoutsFrequency: '5 vezes por semana',
+        goals: 'Definir músculos e manter energia',
+        foodRestrictions: 'Lactose, nozes',
+        foodPreferences: 'Vegetariana, prefere pratos quentes no almoço',
+      }),
+    ),
+
+    create: jest.fn().mockReturnValue(
+      new User({
+        name: 'Fernanda Oliveira',
+        email: 'fernanda.oliveira@example.com',
+        password: 'SenhaForte2024',
+        birthday: '1995-04-10',
+        weight: 60,
+        height: 167,
+        workoutsFrequency: '5 vezes por semana',
+        goals: 'Definir músculos e manter energia',
+        foodRestrictions: 'Lactose, nozes',
+        foodPreferences: 'Vegetariana, prefere pratos quentes no almoço',
+      }),
+    ),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, JwtService, UsersService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
