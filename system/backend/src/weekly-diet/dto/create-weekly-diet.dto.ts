685f5eedd5d4a9ecc3d75a806ad6542422f4560a
Ian-Cerqueira <iandavi16@outlook.com>
2025-07-05 12:42:12 -0300

    docs: Adiciona documentação sobre endpoint de criação de dieta

--- system/backend/src/weekly-diet/dto/create-weekly-diet.dto.ts
+++ system/backend/src/weekly-diet/dto/create-weekly-diet.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateWeeklyDietDto {
+  @ApiProperty({
+    description:
+      'Utilizado para criar a dieta baseaada nas informações ligadas ao respectivo ID',
+    example: 1,
+  })
   @IsNotEmpty()
   @IsNumber()
   userId: number;
