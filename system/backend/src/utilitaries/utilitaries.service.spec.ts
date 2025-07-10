41b10f0aba313265edc73f89eee891158bb587e8
Ian-Cerqueira <168128013+Ian-Cerqueira@users.noreply.github.com>
2025-07-10 16:06:17 +0000

    test: Adição inicial do teste para o utilitaries

--- system/backend/src/utilitaries/utilitaries.service.spec.ts
+++ system/backend/src/utilitaries/utilitaries.service.spec.ts
@@ -15,4 +15,24 @@ describe('UtilitariesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Quando chamada a função encode', () => {
+    it('deveria retornar um UpdateUserDto com a senha criptografada', async () => {
+      const entryDto = {
+        name: 'Fernanda Oliveira',
+        email: 'fernanda.oliveira@example.com',
+        password: 'SenhaForte2024',
+        birthday: '1995-04-10',
+        weight: 60,
+        height: 167,
+        workoutsFrequency: '5 vezes por semana',
+        goals: 'Definir músculos e manter energia',
+        foodRestrictions: 'Lactose, nozes',
+        foodPreferences: 'Vegetariana, prefere pratos quentes no almoço',
+      }
+      const newDto = service.encode(entryDto)
+      expect((await newDto).password).
+    });
+  });
+
 });
